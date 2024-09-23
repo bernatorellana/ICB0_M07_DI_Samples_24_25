4b6f203d170d5aa7113ebc2257f3e104a1734d3d
developrepogithub <develop.repo.github@gmail.com>
2024-09-23 19:45:30 +0200

    Repo creat

---
+++ UF1/20240918_csharp/IntroCsharp/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace IntroCsharp
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private int fantasticaVariable = 123;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            int variableLocalissima = 234;
+            txbSortida.Text = "Hola bon dia";
+            Debug.WriteLine("Saludos desde el Averno");
+
+
+
+            int i = 0;
+            float f = 234.3f;
+
+            decimal nomina = 234.23M;
+
+            var cosaQueMeDaLaGana = 123;
+
+            String nom = "Paquito";
+            nom = nom + " Mas Cositas";
+
+            String missatge = $"Estimado señor {nom}, vagi vosté a parir panteres.";
+
+
+            String cadenaMoltLlarga = @"SELECT * FROM  
+                                        EMP WHERE EMP_NO = 123
+                                        AND OFICI = 'COMERCIAL'";
+
+
+            String ip = "123.12.43.123";
+            String[] octets = ip.Split(".");
+
+            String numero = "2323";
+            numero = numero.PadLeft(10, ' ');
+
+            
+            txbSortida.Text = missatge;
+
+            //---------------------------------------------------------
+            // Conversió de número a cadena
+            float numeraco = 234.234f;
+            // Conversió usant l'idioma per defecte
+            txbSortida.Text = "" + numeraco.ToString("#########.00");
+            // Conversió usant l'idioma customitzat
+            CultureInfo us = new CultureInfo("en-US");
+            txbSortida.Text = ""+numeraco.ToString("#########.00",us);
+            //---------------------------------------------------------
+            decimal resultat = 0;
+            if(Decimal.TryParse("123,7", out resultat))
+            {
+                txbSortida.Text = "Felicitats";
+            } else
+            {
+                Debug.WriteLine("Maleït número !!!");
+            }
+            //---------------------------------------------------------
+            DateTime ara = DateTime.Now;
+            DateTime avui = DateTime.Today;
+            DateTime unaData = new DateTime(2025, 09, 12, 4, 56, 23);
+            CultureInfo ca = new CultureInfo("en-UK");
+            String dataAmbFormat = ara.ToString("dd/MMMM/yyyy HH:mm:ss",ca);
+            txbSortida.Text = dataAmbFormat;
+            //--------------------------------------------------------------
+            try
+            {
+                DateTime laData = DateTime.ParseExact(dataAmbFormat, "dd/MMMM/yyyy HH:mm:ss", ca);
+            }
+            catch(Exception e)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ UF1/20240919_validacions/AppValidacions/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.ViewManagement.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace AppValidacions
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<int> list = new List<int>();
+            for (int dia = 1; dia < 32; dia++)
+            {
+                list.Add(dia);
+            }
+            cboDia.ItemsSource = list;
+            //*********************************************
+            List<string> mesos = new List<string>();
+            DateTime dateTime = new DateTime(2024, 01, 01);
+            for (int i = 0; i < 12; i++)
+            {
+                mesos.Add(dateTime.ToString("MMMM"));
+                dateTime = dateTime.AddMonths(1);
+            }
+            cboMes.ItemsSource = mesos;
+
+            //***********************************************
+            DateTime avui = DateTime.Today;
+            List<int> anys = new List<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                anys.Add(avui.Year);
+                avui = avui.AddYears(1);
+            }
+            cboAny.ItemsSource = anys;
+
+        }
+
+        // Caràcters / icones d'error i ok
+        private const string ICON_OK = "";
+        private const string ICON_ERR = "";
+
+
+        private void txbAlcada_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            float alcada;
+            bool ok = float.TryParse(txbAlcada.Text, out alcada);
+            mostrarIcona(ok, txtAlcadaIcon);
+        }
+
+        private void mostrarIcona(bool ok, TextBlock txtIcon)
+        {
+            txtIcon.Text = ok ? ICON_OK : ICON_ERR;
+            txtIcon.Foreground = new SolidColorBrush(ok ? Colors.Green : Colors.Red);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int MapVirtualKey(int uCode, uint uMapType);
+        private void txbAlcada_PreviewKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+
+            char c = (char)MapVirtualKey((int)e.Key, (uint)2);
+
+            //e.OriginalKey == Windows.System.VirtualKey.Number0
+            e.Handled = !(e.OriginalKey == Windows.System.VirtualKey.Number0);
+            //e.Handled= true; // true intercepta la tecla
+        }
+
+        private void txbDataNaix_LostFocus(object sender, RoutedEventArgs e)
+        {
+            CultureInfo culture = CultureInfo.GetCultureInfo("es-ES");
+            DateTime data;
+            bool ok = DateTime.TryParseExact(
+                txbDataNaix.Text, 
+                "dd/MM/yyyy", 
+                culture,
+                DateTimeStyles.None,
+                out data);
+            mostrarIcona(ok, txtDataNaixIcon);
+
+            if (ok) {
+                cboDia.SelectedValue = data.Day;
+                cboMes.SelectedValue = data.ToString("MMMM");
+                cboAny.SelectedValue = data.Year;
+            }
+        }
+    }
+    }
