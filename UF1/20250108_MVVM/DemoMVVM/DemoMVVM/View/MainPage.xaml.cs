e8ca6e0f52a419f2bfc2f46a2e28fe5e67877d84
developrepogithub <develop.repo.github@gmail.com>
2025-01-08 21:28:23 +0100

    Base inicial MVVM

---
+++ UF1/20250108_MVVM/DemoMVVM/DemoMVVM/View/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DemoMVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace DemoMVVM
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MainPageViewModel viewModel;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.viewModel = new MainPageViewModel();
+            // IMPORTANT: posem les dades al context de la pàgina
+            // de forma que el Binding el detecta automàticament
+            this.DataContext = viewModel;
+        }
+    }
+}
---
+++ UF1/20250108_MVVM/DemoMVVM/DemoMVVM/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DemoMVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoMVVM.ViewModel
+{
+    internal class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+		private ObservableCollection<Persona> persones;
+
+		public ObservableCollection<Persona> Persones
+		{
+			get { return persones; }
+            set { persones = value; }
+		}
+
+        
+        public MainPageViewModel()
+        {
+                Persones = Persona.GetPersones();
+        }
+    }
+}
